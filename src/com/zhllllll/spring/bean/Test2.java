2bac92c9192365ec58c4ed98fcfeaccf873dd03d
zhl <zenghoulu@163.com>
2022-09-14 14:47:57 +0800

    commit three

---
+++ src/com/zhllllll/spring/bean/Test2.java
@@ -0,0 +1,48 @@
+package com.zhllllll.spring.bean;
+
+import java.util.PrimitiveIterator;
+
+public class Test2 {
+    private  String test;
+    private  boolean flag;
+    private  Integer id;
+
+    @Override
+    public String toString() {
+        return "Test2{" +
+                "test='" + test + '\'' +
+                ", flag=" + flag +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Test2(String test, boolean flag, Integer id) {
+        this.test = test;
+        this.flag = flag;
+        this.id = id;
+    }
+}
